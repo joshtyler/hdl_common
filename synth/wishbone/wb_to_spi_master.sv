ae2ef555c15c296e99783ee65f79760ef53da1f2
Joshua Tyler <josh.tyler@btinternet.com>
2020-03-08 16:30:37 +0000

    Update wb to spi bridge to inject and extract data

--- synth/wishbone/wb_to_spi_master.sv
+++ synth/wishbone/wb_to_spi_master.sv
@@ -23,25 +23,28 @@ module wb_to_spi_master
 	output logic mosi
 );
 
-logic [BYTES*8-1:0]   wb_axis_dat_m2s , wb_config_dat_m2s;
-logic [BYTES*8-1:0]   wb_axis_dat_s2m , wb_config_dat_s2m;
-logic                 wb_axis_we      , wb_config_we     ;
-logic                 wb_axis_stb     , wb_config_stb    ;
-logic                 wb_axis_cyc     , wb_config_cyc    ;
-logic                 wb_axis_ack     , wb_config_ack    ;
-logic                 wb_axis_stall   , wb_config_stall  ;
+logic [BYTES*8-1:0]   wb_axis_dat_m2s , wb_config_dat_m2s , wb_inject_dat_m2s;
+logic [BYTES*8-1:0]   wb_axis_dat_s2m , wb_config_dat_s2m , wb_inject_dat_s2m;
+logic                 wb_axis_we      , wb_config_we      , wb_inject_we     ;
+logic                 wb_axis_stb     , wb_config_stb     , wb_inject_stb    ;
+logic                 wb_axis_cyc     , wb_config_cyc     , wb_inject_cyc    ;
+logic                 wb_axis_ack     , wb_config_ack     , wb_inject_ack    ;
+logic                 wb_axis_stall   , wb_config_stall   , wb_inject_stall  ;
+
 
 wb_interconnect
 #(
-	.NUM_MASTERS(2),
+	.NUM_MASTERS(3),
 	.ADDR_BITS(ADDR_BITS),
 	.BYTES(BYTES),
 	.SEL_WIDTH(1),
 	.MASTER_ADDRESSES({
+		{(ADDR_BITS-2){1'b0}},2'b11,
 		{(ADDR_BITS-2){1'b0}},2'b10,
 		{(ADDR_BITS-2){1'b0}},2'b01
 	}),
 	.MASTER_ADDRESS_MASKS({
+		{(ADDR_BITS){1'b1}},
 		{(ADDR_BITS){1'b1}},
 		{(ADDR_BITS){1'b1}}
 	})
@@ -57,22 +60,23 @@ wb_interconnect
 	.s_wb_stall  (s_wb_stall  ),
 
 	.m_wb_addr(),
-	.m_wb_dat_m2s({wb_axis_dat_m2s , wb_config_dat_m2s}),
-	.m_wb_dat_s2m({wb_axis_dat_s2m , wb_config_dat_s2m}),
-	.m_wb_we     ({wb_axis_we      , wb_config_we     }),
+	.m_wb_dat_m2s({wb_inject_dat_m2s , wb_axis_dat_m2s , wb_config_dat_m2s }),
+	.m_wb_dat_s2m({wb_inject_dat_s2m , wb_axis_dat_s2m , wb_config_dat_s2m }),
+	.m_wb_we     ({wb_inject_we      , wb_axis_we      , wb_config_we      }),
 	.m_wb_sel    (),
-	.m_wb_stb    ({wb_axis_stb     , wb_config_stb    }),
-	.m_wb_cyc    ({wb_axis_cyc     , wb_config_cyc    }),
-	.m_wb_ack    ({wb_axis_ack     , wb_config_ack    }),
-	.m_wb_stall  ({wb_axis_stall   , wb_config_stall  })
+	.m_wb_stb    ({wb_inject_stb     , wb_axis_stb     , wb_config_stb     }),
+	.m_wb_cyc    ({wb_inject_cyc     , wb_axis_cyc     , wb_config_cyc     }),
+	.m_wb_ack    ({wb_inject_ack     , wb_axis_ack     , wb_config_ack     }),
+	.m_wb_stall  ({wb_inject_stall   , wb_axis_stall   , wb_config_stall   })
 );
+
 
 logic [7:0] config_reg;
 simple_wb_slave
 #(
 	.BYTES(BYTES),
 	.ADDR_BITS(0),
-	.INITAL_VAL('1)
+	.INITAL_VAL(8'b00000001) // CS high, don't discard RX data
 ) cs_inst (
 	.clk(clk),
 	.sresetn(sresetn),
@@ -87,6 +91,8 @@ simple_wb_slave
  	.s_wb_stall  (wb_config_stall  ),
 	.regs(config_reg)
 );
+logic discard_rx_data;
+//assign discard_rx_data = config_reg[1];
 assign ss = config_reg[0];
 
 logic       axis_wb_to_spi_tready;
@@ -100,6 +106,36 @@ logic [7:0] axis_spi_to_wb_tdata;
 logic       axis_spi_to_wb_buf_tready;
 logic       axis_spi_to_wb_buf_tvalid;
 logic [7:0] axis_spi_to_wb_buf_tdata;
+
+// Register to inject bytes into stream
+// When the counter is non zero, inject that many dummy bytes into the stream
+// To make the logic simple, assume that we are not trying to write at the same time
+logic [7:0] inject_ctr;
+always_ff @(posedge clk)
+begin
+	if (!sresetn)
+	begin
+		inject_ctr <= 0;
+	end else begin
+
+		if (wb_inject_stb && wb_inject_we)
+		begin
+			inject_ctr<= wb_inject_dat_m2s;
+		end else if (axis_wb_to_spi_tready && (inject_ctr != 0))  begin
+			inject_ctr<= inject_ctr-1;
+		end
+	end;
+
+	wb_inject_dat_s2m <= inject_ctr;
+
+	// Acknowledge reads
+	wb_inject_ack <= wb_inject_stb;
+end
+// We never need to stall
+assign wb_inject_stall = 0;
+
+
+
 
 wb_axis_bridge
 #(
@@ -134,7 +170,7 @@ axis_fifo
 	.sresetn(sresetn),
 
 	.axis_i_tready(axis_spi_to_wb_tready),
-	.axis_i_tvalid(axis_spi_to_wb_tvalid),
+	.axis_i_tvalid(axis_spi_to_wb_tvalid && !(discard_rx_data)), // If we are discarding RX data, don't save the read data into the buffer
 	.axis_i_tlast(1),
 	.axis_i_tdata (axis_spi_to_wb_tdata),
 
@@ -153,7 +189,7 @@ axis_spi_bridge
 	.sresetn(sresetn),
 
 	.axis_i_tready(axis_wb_to_spi_tready),
-	.axis_i_tvalid(axis_wb_to_spi_tvalid),
+	.axis_i_tvalid(axis_wb_to_spi_tvalid || (inject_ctr != 0)), // We're commanded to inject
 	.axis_i_tdata(axis_wb_to_spi_tdata),
 
 	.axis_o_tready(axis_spi_to_wb_tready),
