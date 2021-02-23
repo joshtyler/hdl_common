b4693bedc4bcadefc7cf87f32825b738aebc83fd
Joshua Tyler <josh.tyler@btinternet.com>
2021-02-23 21:51:03 +0000

    Untested draft of GMII source model

---
+++ sim/network/GMIISource.cpp
@@ -0,0 +1,58 @@
+#include "GMIISource.hpp"
+
+void GMIISource::eval(void)
+{
+    if((clk->getEvent() == ClockGen::Event::RISING))
+    {
+        // Setup no data
+        eth_rxdv = 0;
+        eth_rxer = 0;
+
+        // If we have run out of data, try and get more
+        if (iter == current_packet.end())
+        {
+            auto maybe_new_packet = data_source->receive();
+            if (maybe_new_packet)
+            {
+                current_packet = *maybe_new_packet;
+
+                // Pad if less than minimum size
+                if (current_packet.size() < 60)
+                {
+                    // Pad with zeros, even though actual value does not matter
+                    current_packet.resize(60 - current_packet.size(), 0);
+                }
+
+                // Add the ethernet CRC to the front
+                uint32_t crc = crc32(0, current_packet.data(), current_packet.size());
+                boost::endian::native_to_big_inplace(crc);
+                auto crc_u8_ptr = reinterpret_cast<uint8_t *>(&crc);
+                std::copy(crc_u8_ptr, crc_u8_ptr + 4, std::back_inserter(current_packet));
+
+                // Put preamble on the front
+                current_packet.insert(current_packet.begin(), preamble.begin(), preamble.end());
+
+                iter = current_packet.begin();
+            }
+        }
+
+        // Enforce the inter-packet gap if we just finished a packet
+        if (ipg_counter)
+        {
+            ipg_counter--;
+        } else {
+            // If we have data to give, present that
+            if (iter != current_packet.end())
+            {
+                eth_rxdv = 1;
+                eth_rxd = *(iter++);
+
+                if (iter == current_packet.end())
+                {
+                    ipg_counter = 12;
+                }
+            }
+        }
+    }
+}
+
---
+++ sim/network/GMIISource.hpp
@@ -0,0 +1,50 @@
+//  Copyright (C) 2021 Joshua Tyler
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 2.1 of the License, or (at your option) any later version.
+//  See the file LICENSE_LGPL included with this distribution for more
+//  information.
+
+#ifndef GMIISOURCE_HPP
+#define GMIISOURCE_HPP
+
+// Output an AXI Stream from a vector of vectors
+// N.B. Currently this does not support any kind of reset
+#include <vector>
+#include "../other/ClockGen.hpp"
+#include "../verilator/Peripheral.hpp"
+#include "../other/PacketSourceSink.hpp"
+#include <zlib.h>
+
+
+class GMIISource : public Peripheral
+{
+public:
+    GMIISource(gsl::not_null<ClockGen *> clk_, gsl::not_null<vluint8_t *>eth_rxd_, gsl::not_null<vluint8_t *>eth_rxdv_, gsl::not_null<vluint8_t *>eth_rxer_, gsl::not_null<PacketSource<vluint8_t> *> data_source_)
+		:clk(clk_), eth_rxd(eth_rxd_), eth_rxdv(eth_rxdv_), eth_rxer(eth_rxer), data_source(data_source_)
+	{
+        eth_rxd = 0;
+        eth_rxdv = 0;
+        eth_rxer = 0;
+	};
+
+	void eval(void) override;
+
+private:
+	ClockGen *clk;
+    OutputWrapper<vluint8_t> eth_rxd;
+    OutputWrapper<vluint8_t> eth_rxdv;
+    OutputWrapper<vluint8_t> eth_rxer;
+
+    PacketSource<uint8_t> *data_source;
+    std::vector<uint8_t> current_packet;
+    typename std::vector<uint8_t>::const_iterator iter = current_packet.end();
+
+    unsigned int ipg_counter{0};
+
+    constexpr std::array<uint8_t,8> preamble = {0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0xD5};
+};
+
+#endif
